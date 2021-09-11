53eb7e5d04515e699e453bfe1847c76e167d1334
elham007 <alifelham@gmail.com>
2021-09-11 14:14:05 +0600

    Update urls.py
    Adding pathways to the pages

--- Backend/DjangoProject_1/blog/urls.py
+++ Backend/DjangoProject_1/blog/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('login/', views.login, name='blog-login'),
+    path('payment/', views.payment, name='blog-payment'),
+    path('booking/', views.booking, name='blog-booking'),
+    path('edit-booking/', views.edit_booking, name='blog-edit-booking'),
+    path('travel-packages/', views.travel_packages, name='blog-travel-packages'),
 ]
